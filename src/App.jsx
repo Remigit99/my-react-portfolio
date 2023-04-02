88dc513065c9aa9cb6d863002b344c316583653a
Remigit99 <aderemibiodun1000@gmail.com>
2023-04-02 20:29:13 +0100

    modified header.css

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,20 @@
 import { useState } from 'react'
 
 import './App.css'
+import Nav from './Components/Nav/Nav';
+import Header from './Components/Header/Header';
+import About from './Components/About/About';
 
 const App = () => {
 
 
   return (
-    <div className="App">
+    <div className="container app__container">
 
       <h1>You Are Welcome To My World</h1>
+      <Nav />
+      <Header />
+      <About />
 
     </div>
   )
