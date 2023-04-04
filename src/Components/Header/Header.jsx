466b71bb62488eeea04e1c3bfbf8a9575297c328
Remigit99 <aderemibiodun1000@gmail.com>
2023-04-04 09:01:21 +0100

    modified header.jsx

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,10 +1,42 @@
 import React from 'react';
+import { FiDownload } from 'react-icons/fi'
 import './Header.css';
 
 
 const Header = () => {
     return (
-        <div>Header</div>
+        <main>
+
+            <div className="container header__container">
+
+                <div className="header__top">
+
+                    <h3 className="text-light">
+                        Hi, I am
+                    </h3>
+
+                    <h1>Aderemi Abiodun</h1>
+
+                    <h3 className="text-light">
+                        Frontend Developer
+                    </h3>
+
+                </div>
+
+                <div className="download__talk">
+                    <div className="download">
+                        <span>
+                            <FiDownload />
+                            <a href="#" download ></a>
+                        </span>
+                    </div>
+                    <div className="talk">
+                        <a href="#contact">Let's Talk</a>
+                    </div>
+                </div>
+
+            </div>
+        </main>
     )
 }
 
